8645e1777903332ec58e44b48f0bb532723e6192
sm-le <smlee@seegene.com>
2023-06-14 13:27:06 +0900

    chore: kmer comp

---
+++ bioinformatic_stronghold/string_algorithm/kmer_composition.py
@@ -0,0 +1,45 @@
+"""
+String Algorithm 15
+
+Given a DNA String s in FASTA format, return the 4-mer composition of s
+
+Example
+IN: 
+>fa_1
+CTTCGAAAGTTTGGGCCGAGTCTTACAGTCGGTCTTGAAGCAAAGTAACGAACTCCACGG
+CCCTGACTACCGAACCAGTTGTGAGTACTCAACTGGGTGAGAGTGCAGTCCCTATTGAGT
+TTCCGAGACTCACCGGGATTTTCGATCCAGCCTCAGTCCAGTCTTGTGGCCAACTCACCA
+AATGACGTTGGAATATCCCTGTCTAGCTCACGCAGTACTTAGTAAGAGGTCGCTGCAGCG
+GGGCAAGGAGATCGGAAAATGTGCTCTATATGCGACTAAAGCTCCTAACTTACACGTAGA
+CTTGCCCGTGTTAAAAACTCGGCTCACATGCTGTCTGCGGCTGGCTGTATACAGTATCTA
+CCTAATACCCTTCAGTTCGCCGCACAAAAGCTGGGAGTTACCGCGGAAATCACAG
+
+OUT:
+4 1 4 3 0 1 1 5 1 3 1 2 2 1 2 0 1 1 3 1 2 1 3 1 1 1 1 2 2 5 1 3 0 2 2 1 1 1 1 3 1 0 0 1 5 5 1 5 0 2 0 2 1 2 1 1 1 2 0 1 0 0 1 1 3 2 1 0 3 2 3 0 0 2 0 8 0 0 1 0 2 1 3 0 0 0 1 4 3 2 1 1 3 1 2 1 3 1 2 1 2 1 1 1 2 3 2 1 1 0 1 1 3 2 1 2 6 2 1 1 1 2 3 3 3 2 3 0 3 2 1 1 0 0 1 4 3 0 1 5 0 2 0 1 2 1 3 0 1 2 2 1 1 0 3 0 0 4 5 0 3 0 2 1 1 3 0 3 2 2 1 1 0 2 1 0 2 2 1 2 0 2 2 5 2 2 1 1 2 1 2 2 2 2 1 1 3 4 0 2 1 1 0 1 2 2 1 1 1 5 2 0 3 2 1 1 2 2 3 0 3 0 1 3 1 2 3 0 2 1 2 2 1 2 3 0 1 2 3 1 1 3 1 0 1 1 3 0 2 1 2 2 0 2 1 1
+"""
+
+from itertools import product
+from tools.fasta_reader import FastaFile
+path = "example_dataset/rosalind_kmer.txt"
+k = 4
+
+def lexicographic_kmer(path:str, k:int) -> str:
+    """Print count array of lexicographically ordered acgt 
+    after reading sequence from a given path
+
+    Args:
+        path: fasta file path
+        k: kmer size
+
+    Returns:
+        str(jointed array)
+    """
+    letters = ['A','C','G','T']
+    lex_kmers = {''.join(i):0 for i in product(letters,repeat=4)}
+
+    with FastaFile(path) as fa:
+        for record in fa.body:
+            for i in range(len(record["sequence"])-k+1):
+                lex_kmers[record["sequence"][i:i+4]] += 1
+    print(" ".join(list(map(str,lex_kmers.values()))))
+    return lex_kmers
