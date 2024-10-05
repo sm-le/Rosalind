0194cc451e58dc7a6e3afadc0d44a8f087135aeb
sm-le <sungmin.lee.bio@gmail.com>
2024-10-04 22:39:20 -0700

    add: test plan

--- others/test.py
+++ others/test.py
@@ -1,5 +1,8 @@
 from fib import fib
+from ndArrayFlatten import flattenArr
+from countTriplet import triplet_counter
 import unittest
+
 
 class testAlg(unittest.TestCase):
     @classmethod
@@ -16,6 +19,19 @@ class testAlg(unittest.TestCase):
         self.assertEqual(fib(10),55)
         # empty input
         self.assertEqual(fib(),5)
+    
+    def setUp(self):
+        self.arr1 = [1,2,3,[4,5]]
+        self.arr2 = [[1,2],3,[4,5]]
+        self.arr3 = [[1,[2,3]],4,5]
+    def test_flattenArr(self):
+        self.assertEqual(flattenArr(self.arr1),[1,2,3,4,5])
+        self.assertEqual(flattenArr(self.arr2),[1,2,3,4,5])
+        self.assertEqual(flattenArr(self.arr3),[1,2,3,4,5])
+
+    def test_triplet_counter(self):
+        self.assertEqual(triplet_counter([1, 2, 3, 4, 5],6),1)
+        self.assertEqual(triplet_counter([10, 10, 20, 30, 40],60),3)
 
 if __name__ == '__main__':
     unittest.main()
